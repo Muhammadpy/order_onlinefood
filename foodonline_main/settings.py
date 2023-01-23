88c645bebf4b8c5f430b47549c911b832c93ec92
nurmukhamad <99017326+Muhammadpy@users.noreply.github.com>
2023-01-23 19:21:45 +0900

    forgot password and reset password

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -142,3 +142,13 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+# Email configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = 'acwgyciqvbyywxpk'
+# config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'foodonline marketplace <sharobidinovnurmukhamad94@gmail.com>'
